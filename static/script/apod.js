9d7a18f7614aea61819ec81048343e8ac2027e2d
pepega8887 <derek.io@outlook.com>
2022-08-03 21:45:36 +1000

    Working on bottom page.

--- static/script/apod.js
+++ static/script/apod.js
@@ -14,20 +14,25 @@ function checkKey(e) {
     }
 }
 
-var move_date
+var move_date = 1;
 
-function fetch_images(yesterday, today, tomorrow, option=null) {
+function fetch_images(yesterday, today, tomorrow) {
     fetch(`/.netlify/functions/fetch_images?start_date=${yesterday}&today_date=${today}&tomorrow_date=${tomorrow}`)
         .then(res => res.json())
         .then(data => {
-            if (option != null) {
-                return data
-            }
+            console.log(move_date);
 
             if (data.length == 1) {
                 move_back()
             }
-            
+
+            if (move_date == 1) {
+                document.getElementById("move-forward").disabled = true;
+                document.getElementById("tmrw-img").src = "static/imgs/red-gradient.gif";
+            } else {
+                document.getElementById("move-forward").disabled = false;
+            }
+
             document.getElementById("title-text").innerHTML = data[1]["title"];
 
             var photographer = "placeholder";
@@ -105,87 +110,92 @@ function fetch_images(yesterday, today, tomorrow, option=null) {
 
                 document.getElementById("tmrw-img").src = data[2]["hdurl"];
             }
-
-            if (data.length == 2) {
-                document.getElementById("move-forward").disabled = true;
-            }
-
+            
             if (data.length == 3) {
                 document.getElementById("tmrw-img").src = data[2]["hdurl"];
             }
+
             })
 }
 
 
 function move_back() {
-    let today = new Date();
+    let x = new Date();
+    let tmrw = new Date();
     let yesterday = new Date();
 
     move_date += 1;
 
-    yesterday.setDate(today.getDate() - move_date);
+    yesterday.setDate(x.getDate() - move_date);
+    tmrw.setDate(yesterday.getDate() + 2)
     const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-    var today_day = String(today.getDate());
-    var today_month = String(months[today.getMonth()]);
-    var today_year = String(today.getFullYear());
+    var tmrw_day = String(tmrw.getDate());
+    var tmrw_month = String(months[tmrw.getMonth()-1]);
+    var tmrw_year = String(tmrw.getFullYear());
     var yesterday_day = String(yesterday.getDate());
     var yesterday_month = String(months[yesterday.getMonth()]);
     var yesterday_year = String(yesterday.getFullYear());
 
 
-    if (today_day.length == 1) {
-        today_day = `0${today_day}`
+    if (tmrw_day.length == 1) {
+        tmrw_day = `0${tmrw_day}`
     }
     if (yesterday_day.length == 1) {
         yesterday_day = `0${yesterday_day}`
     }
-    if (today_month.length == 1) {
-        today_month = `0${today_month}`
+    if (tmrw_month.length == 1) {
+        tmrw_month = `0${tmrw_month}`
     }
     if (yesterday_month.length == 1) {
         yesterday_month = `0${yesterday_month}`
     };
 
     const yesterday_arg = `${yesterday_year}-${yesterday_month}-${yesterday_day}`;
-    const today_arg = `${today_year}-${today_month}-${today_day}`
-    fetch_images(yesterday_arg, today_arg, "n");
+    const tmrw_arg = `${tmrw_year}-${tmrw_month}-${tmrw_day}`
+    fetch_images(yesterday_arg, "n", tmrw_arg);
 }
 
 function move_forward() {
-    let x = new Date()
-    let today = new Date();
+    let x = new Date();
+    let yesterday = new Date();
     let tmrw = new Date();
-    
-    today.setDate(x.getDate() - move_date);
+
 
     move_date -= 1;
+    
+    yesterday.setDate(x.getDate() - move_date);
 
-    tmrw.setDate(today.getDate() - move_date);
+    tmrw.setDate(yesterday.getDate() + 2);
     const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-    var today_day = String(today.getDate());
-    var today_month = String(months[today.getMonth()]);
-    var today_year = String(today.getFullYear());
+    var yesterday_day = String(yesterday.getDate());
+    var yesterday_month = String(months[yesterday.getMonth()]);
+    var yesterday_year = String(yesterday.getFullYear());
     var tmrw_day = String(tmrw.getDate());
-    var tmrw_month = String(months[tmrw.getMonth()]);
+    var tmrw_month = String(months[tmrw.getMonth()-1]);
     var tmrw_year = String(tmrw.getFullYear());
 
 
-    if (today_day.length == 1) {
-        today_day = `0${today_day}`
+    if (yesterday_day.length == 1) {
+        yesterday_day = `0${yesterday_day}`
     }
     if (tmrw_day.length == 1) {
         tmrw_day = `0${tmrw_day}`
     }
-    if (today_month.length == 1) {
-        today_month = `0${today_month}`
+    if (yesterday_month.length == 1) {
+        yesterday_month = `0${yesterday_month}`
     }
     if (tmrw_month.length == 1) {
         tmrw_month = `0${tmrw_month}`
     };
 
     const tmrw_arg = `${tmrw_year}-${tmrw_month}-${tmrw_day}`;
-    const today_arg = `${today_year}-${today_month}-${today_day}`
-    fetch_images(today_arg, tmrw_arg, "n");
+    const yesterday_arg = `${yesterday_year}-${yesterday_month}-${yesterday_day}`
+
+    if (move_date == 1) {
+        fetch_images(yesterday_arg, "n", "n");
+    } else {
+        fetch_images(yesterday_arg, "n", tmrw_arg);
+    }
 }
 
 
@@ -220,5 +230,4 @@ if (yesterday_month.length == 1) {
 
 const yesterday_arg = `${yesterday_year}-${yesterday_month}-${yesterday_day}`;
 const today_arg = `${today_year}-${today_month}-${today_day}`
-move_date = 1
 fetch_images(yesterday_arg, today_arg, "n");
